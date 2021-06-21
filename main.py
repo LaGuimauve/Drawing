ed3fe37f8f919e4bb4baa29560e52309f660e20b
Patrick <messager.patrick428@gmail.com>
2021-06-21 20:25:03 +0200

    +white square

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import turtle
 
 
-def square(color):
+def square(color='black'):
     turtle.fillcolor(color)
     turtle.begin_fill()
     for i in range(4):
@@ -11,39 +11,49 @@ def square(color):
     turtle.end_fill()
 
 
-def guniv():
+def avancer(forward=100, angle=90, direction="droite", avant=False):
+    if avant:
+        turtle.forward(forward)
+
+    if direction == "droite":
+        turtle.right(angle)
+    else:
+        turtle.left(angle)
+
+    if not avant:
+        turtle.forward(forward)
+
+
+def main():
     turtle.fillcolor('white')
     turtle.begin_fill()
     for i in range(4):
-        turtle.forward(200)
-        turtle.right(90)
+        avancer(forward=200, avant=True)
     turtle.end_fill()
     turtle.up()
-    turtle.right(90)
-    turtle.forward(100)
+    avancer()
     turtle.left(90)
     turtle.down()
-    square('black')
+    square()
     turtle.up()
     turtle.forward(175)
     turtle.down()
-    square('black')
+    square()
     turtle.up()
-    turtle.right(90)
-    turtle.forward(50)
-    turtle.right(90)
-    turtle.forward(25)
+    avancer(forward=50)
+    avancer(forward=25)
     turtle.down()
     for i in range(4):
-        square('black')
+        square()
         turtle.forward(25)
-    turtle.left(180)
-    turtle.forward(50)
+    avancer(forward=50, angle=180)
     turtle.right(90)
-    square('black')
+    square()
     turtle.left(90)
     square('red')
     turtle.forward(25)
     square('red')
     turtle.done()
-guniv()
+
+
+main()
